e1287a075318dabd2c996f0e38a8d350fac509fa
Geovanny <birdcode.py@gmail.com>
2024-05-07 22:14:38 -0500

    Adicion de comentarios al programa de resolucion de ecuaciones con estructura aditiva y multiplicativa

--- Ecuaciones_aditivas.py
+++ Ecuaciones_aditivas.py
@@ -1,3 +1,6 @@
+# Practica construcción de función para resolver ecuaciones con estructura aditiva y multiplicativa combinadass
+
+
 def ecuacion_aditiva(a, b, c, d):
 
 	x = 0
@@ -6,6 +9,9 @@ def ecuacion_aditiva(a, b, c, d):
 	x = der / (c + (-d))
 
 	print("El valor de la incognita es: " ,x)
+
+
+# Llamada a la función para resolver la ecuación 81 - 3x + 2 - 5x = 91 + 7x + 2 + 2x
 
 
 ecuacion_aditiva(81+2, 91+2, -3-5, 7+2)
