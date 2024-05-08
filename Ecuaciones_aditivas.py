8209740f43aecb7eb667b5322afceea9fb9124fc
Geovanny <birdcode.py@gmail.com>
2024-05-07 23:10:29 -0500

    Cuarto ajuste de comentarios

--- Ecuaciones_aditivas.py
+++ Ecuaciones_aditivas.py
@@ -1,4 +1,9 @@
-# Practica construcción de función para resolver ecuaciones con estructura aditiva y multiplicativa combinadas
+
+# ---------------------------------------------------------------------------------------------------------------------------------------
+
+
+#         PRÁCTICA PARA LA CONSTRUCCIÓN DE FUNCIÓN PARA RESOLVER ECUACIONES CON ESTRUCTURA ADITIVA Y MULTIPLICATIVA COMBINADAS
+
 
 ## Notas:
 
@@ -8,31 +13,36 @@
 ### En el paso 4 expresamos el producto de x (incognita) con los coeficientes conocidos y factorizados como cociente de numeros reales.
 ### En el paso 5 despejamos la incognita en el lado izquierdo de la ecuación.
 ### En el paso 6 igualamos la incognita con el lado derecho de la ecuación que expresa las operaciones aditivas y multiplicativas para 
-### los coerficioentes conocidos.
+### los coeficientes conocidos.
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------------
+
+
 
 
 def ecuac_aditiva_multiplicativa(a, b, c, d):
 
 	x = 0
-	izq = a + (-a) + c*x   # Paso 1: Desarrollo de código para modelar el lado izquierdo de la ecuación
-	der = b + (-a) + d*x   # Paso 1: Desarrollo de código para modelar el lado derechos de la ecuación
+	izq = a + (-a) + c*x   # ---------------- Paso 1: Desarrollo de código para modelar el lado izquierdo de la ecuación
+	der = b + (-a) + d*x   # ---------------- Paso 1: Desarrollo de código para modelar el lado derecho de la ecuación
 
-	izq = c*x - d*x    # Paso 2
-	der = b + (-a) + d*x - d*x   # Paso 2
+	izq = c*x - d*x  #----------------------- Paso 2
+	der = b + (-a) + d*x - d*x   # ---------- Paso 2
 
-	izq = x*(c + (-d))   # Paso 3
-	der = b + (-a)      # Paso 3
+	izq = x*(c + (-d))   # ------------------ Paso 3
+	der = b + (-a)      # ------------------- Paso 3
 
-	izq = x*(c + (-d)) / (c + (-d))   # Paso 4
-	der = (b + (-a)) / (c + (-d))      # Paso 4
+	izq = x*(c + (-d)) / (c + (-d))  # ------ Paso 4
+	der = (b + (-a)) / (c + (-d))  # -------- Paso 4
 
-	izq = x                           # Paso 5
-	der = (b + (-a)) / (c + (-d))
+	izq = x   # ----------------------------- Paso 5
+	der = (b + (-a)) / (c + (-d)) # --------- Paso 5
 
 
-	x = der   # Paso 6 - final para el despeje de x como incognita a calcular
+	x = der   # ----------------------------- Paso 6 - final para el despeje de x como incognita a calcular
 
-	print("El valor de la incognita es: " , x)
+	print("El valor de la incognita es: " , x) # -------- Impresión del resultado de la resolución de la ecuación (Valor de la incognita)
 
 
 # Llamada a la función para resolver la ecuación 81 - 3x + 2 - 5x = 91 + 7x + 2 + 2x
